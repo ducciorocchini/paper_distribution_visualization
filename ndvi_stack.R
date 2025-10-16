639aefa06e97aef37c4e18930eb21ef962c65bfe
Duccio Rocchini <ducciorocchini@gmail.com>
2025-10-16 15:49:35 +0200

    Create ndvi_stack.R

---
+++ ndvi_stack.R
@@ -0,0 +1,68 @@
+setwd("~/Downloads/ndvi")
+
+library(terra)
+
+
+ndvi01jan <- rast("Sentinel2_2020-01-24_ndvi.tif")
+ndvi02feb <- rast("Sentinel2_2020-02-21_ndvi.tif ")
+ndvi03mar <- rast("Sentinel2_2020-03-12_ndvi.tif")
+ndvi04apr <- rast("Sentinel2_2020-04-11_ndvi.tif")
+ndvi05may <- rast("Sentinel2_2020-05-08_ndvi.tif")
+ndvi06jun <- rast("Sentinel2_2020-05-21_ndvi.tif")
+ndvi07jul <- rast("Sentinel2_2020-07-05_ndvi.tif")
+ndvi08aug <- rast("Sentinel2_2020-08-01_ndvi.tif")
+ndvi09sep <- rast("Sentinel2_2020-09-05_ndvi.tif")
+ndvi10oct <- rast("Sentinel2_2020-10-10_ndvi.tif")
+ndvi11nov <- rast("Sentinel2_2020-11-27_ndvi.tif")
+ndvi12dec <- rast("Sentinel2_2020-12-17_ndvi.tif")
+
+extent_ref <- ext(ndvi01jan)
+
+ndvi02feb <- crop(ndvi02feb, extent_ref)
+
+# export feb image
+writeRaster(ndvi02feb, "Sentinel2_2020-02-21_ndvi.tif", overwrite=TRUE)
+
+ndvi <- c(ndvi01jan, ndvi02feb, ndvi03mar, ndvi04apr, ndvi05may, ndvi06jun, ndvi07jul, ndvi08aug, ndvi09sep, ndvi10oct, ndvi11nov, ndvi12dec)
+
+plot(ndvi)
+ndvi02feb
+
+# export ndvi
+writeRaster(ndvi, "ndvi_2020.tif", overwrite=TRUE)
+
+# import ndvi
+ndvi <- rast("ndvi_2020.tif")
+
+# show the names for every image in ndvi
+names(ndvi)
+
+# inside ndvi rename the february image
+names(ndvi)[2] <- "Sentinel2_2020-02-21_ndvi"
+
+names(ndvi)
+
+# ndvi_clump <- clumps(ndvi, directions=8)
+
+# plot(ndvi_clump)
+
+# Write the clumped raster to a file
+# writeRaster(ndvi_clump, "ndvi_clump.tif", overwrite=TRUE)
+
+# Load the clumped raster from the file
+# ndvi_clump <- rast("ndvi_clump.tif")
+
+# Calculate the frequency of each clump
+# freq_table <- freq(ndvi_clump)
+# print(freq_table)
+
+# Calculate the area of each clump
+# pixel_area <- res(ndvi_clump)[1] * res(ndvi_clump)[2]  # Area of a single pixel
+# freq_table <- as.data.frame(freq_table)
+# freq_table$area <- freq_table$count * pixel_area
+
+# Print the area of each clump
+# print(freq_table)
+# Save the frequency table to a CSV file
+# write.csv(freq_table, "ndvi_clump_areas.csv", row.names=FALSE)
+
