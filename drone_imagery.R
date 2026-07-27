03904d1d9f55295a7fb3b9ab445c3c3acd97a0c2
Duccio Rocchini <ducciorocchini@gmail.com>
2026-07-27 18:49:54 +0200

    Create drone_imagery.R

---
+++ drone_imagery.R
@@ -0,0 +1,24 @@
+library(terra)
+library(viridis)
+
+# Import the UAV RGB image
+canale <- rast("canale.jpg")
+
+# Partition the image into four spectral classes using k-means clustering
+classes <- im.classify(
+  canale,
+  num_clusters = 4,
+  seed = 42
+)
+
+# Compare the distribution of the red band (layer 2) among the four classes
+# using kernel density boxplots
+im.boxplot.classes(
+  canale,
+  classes,
+  layer = 2,
+  density = TRUE,
+  median_labels = TRUE,
+  limits = c(0.01, 0.99),
+  custom_colors = viridis::viridis(4, end = 0.5)
+)
